9d4c4c99b8d0b694cb5b14911e81f309e15bc8d3
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-10-26 13:53:41 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+int main(){
+	
+	int A[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
+	int inf,sup,mit,dato,n=20; 
+	printf("Ingresa el numero a buscar."); 
+	scanf("%d",&dato); 
+	inf=0; 
+	sup=n; 
+	while(inf<=sup){ 
+		mit=(inf+sup)/2; 
+		if (A[mit]==dato){ 
+			printf("dato %d encontrado posicion %d",dato,mit); 
+			break; 
+		} 
+		if (A[mit]>dato){ 
+			sup=mit; 
+			mit=(inf+sup)/2; 
+		} 
+		if (A[mit]<dato){ 
+			inf=mit; 
+			mit=(inf+sup)/2; 
+		}
+	} 
+	return 0; 
+}
