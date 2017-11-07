173f403f3a9c0b871ee8c4fbdbd07391225064fc
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-11-07 11:46:10 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,26 +1,27 @@
-#include<stdio.h> 
-int main(){
-	
-	int A[20]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
-	int inf,sup,mit,dato,n=20; 
-	printf("Ingresa el numero a buscar."); 
-	scanf("%d",&dato); 
-	inf=0; 
-	sup=n; 
-	while(inf<=sup){ 
-		mit=(inf+sup)/2; 
-		if (A[mit]==dato){ 
-			printf("dato %d encontrado posicion %d",dato,mit); 
-			break; 
-		} 
-		if (A[mit]>dato){ 
-			sup=mit; 
-			mit=(inf+sup)/2; 
-		} 
-		if (A[mit]<dato){ 
-			inf=mit; 
-			mit=(inf+sup)/2; 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+
+int inicio = 0;
+	int medio;
+	int final = 5;
+	int s[5] = { 3, 5, 7, 9, 10 };
+	int busqueda = 10;
+	medio = (inicio + final) / 2;
+
+	while (inicio <= final) {
+		medio = (inicio + final) / 2;
+		if (busqueda == s[medio]) {
+			printf("El elemento esta en la posicion: %d \n", medio + 1);
+			break;
+		} else if (s[medio] > busqueda) {
+			final = medio - 1;
+		} else {
+			inicio = medio + 1;
 		}
-	} 
-	return 0; 
+	}
+
+		return EXIT_SUCCESS;
 }
