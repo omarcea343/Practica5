554a3da9c52ccb33e4c92d9fd6086096451272c1
Omar Zuñiga <miguel_omar30@hotmail.com>
2017-11-07 18:46:11 -0600

    final

--- main.c
+++ main.c
@@ -4,21 +4,22 @@ int main(){
 	
 	int inicio = 0;
 	int medio;
-	int final = 5;
-	int s[5] = { 3, 5, 7, 9, 10 };
+	int final = 15;
+	int vector[15] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
 	int busqueda;
 	
-	printf("Ingresa el numero a buscar. \n"); 
+	printf("Este programa usa la busqueda binaria para encontrar la posicion de un numero. \n\n");
+	printf("Ingresa el numero a buscar. \n\n"); 
 	scanf("%d", &busqueda);
 	
 	medio = (inicio + final) / 2;
 	
 	while (inicio <= final) {
 		medio = (inicio + final) / 2;
-		if (busqueda == s[medio]) {
-			printf("dato %d encontrado posicion %d",busqueda, medio + 1);
+		if (busqueda == vector[medio]) {
+			printf("dato %d encontrado en la posicion %d",busqueda, medio + 1);
 			break;
-		} else if (s[medio] > busqueda) {
+		} else if (vector[medio] > busqueda) {
 			final = medio - 1;
 		} else {
 			inicio = medio + 1;
